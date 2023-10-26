4cd6298a6df9ed438678b20ddfaec4bf7b92f079
brunocoronado49 <francorangelcoronado@gmail.com>
2023-10-25 21:34:19 -0600

    bases terminadas

---
+++ 01-bases/app.js
@@ -0,0 +1,9 @@
+"use strict";
+const msg = 'Hello World!';
+console.log(msg);
+const hero = {
+    name: 'Spiderman',
+    age: 25
+};
+hero.age = 50;
+console.log(hero.age + 1);
---
+++ 01-bases/app.ts
@@ -0,0 +1,11 @@
+const msg: string = 'Hello World!';
+console.log(msg);
+
+const hero = {
+    name: 'Spiderman',
+    age: 25
+}
+
+hero.age = 50
+
+console.log(hero.age + 1);
