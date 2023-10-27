c69820e1a42bc5765caaf07e5a106c23865cf801
brunocoronado49 <francorangelcoronado@gmail.com>
2023-10-26 19:03:27 -0600

    enum y void

---
+++ 01-bases/tipos/void.ts
@@ -0,0 +1,13 @@
+(() => {
+    function callBatman(): void {
+        return;
+    }
+
+    const callSuperman = (): void => {
+
+    }
+
+    const a = callSuperman();
+    console.log(a);
+})();
+
